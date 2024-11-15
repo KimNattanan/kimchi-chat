eff170f6446f1807be602577233e443e5e061866
kim <145192439+KimNattanan@users.noreply.github.com>
2024-11-15 08:23:27 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Index() {
   const router=useRouter();
 
   return (
-    <div className="flex h-screen bg-amber-100 justify-center items-center">
+    <div className="flex max-h-screen bg-amber-100 justify-center items-center">
       <button
         className="bg-white text-sky-700 font-bold h-fit w-fit p-4 m-2 shadow-xl rounded-tl-3xl active:bg-transparent transition-colors"
         onClick={()=>router.push('/sign-in')}
